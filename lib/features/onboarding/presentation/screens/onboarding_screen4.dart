2ca67bf43ced0ed726eb87848666f3d81efbea27
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-02-02 21:34:16 +0200

    onboarding screen 4 done

--- lib/features/onboarding/presentation/screens/onboarding_screen4.dart
+++ lib/features/onboarding/presentation/screens/onboarding_screen4.dart
@@ -1,6 +1,8 @@
+import 'package:dawak/core/theme/styles/app_styles.dart';
 import 'package:dawak/features/onboarding/presentation/widgets/animated_subscription_card.dart';
 import 'package:dawak/features/onboarding/presentation/widgets/on_borading_layout.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen4 extends StatelessWidget {
   const OnboardingScreen4({super.key});
@@ -10,7 +12,27 @@ class OnboardingScreen4 extends StatelessWidget {
     return SafeArea(
       child: OnBoradingLayout(
         onboradingBody: Column(
-          children: [Expanded(child: AnimatedSubscriptionCard())],
+          children: [
+            const Expanded(child: AnimatedSubscriptionCard()),
+            SizedBox(height: 220.h),
+            Expanded(
+              child: Column(
+                children: [
+                  const Text(
+                    'Never Miss a Dose',
+                    style: AppStyles.bold32black,
+                    textAlign: TextAlign.center,
+                  ),
+
+                  Text(
+                    ' Set automatic reminders for your medications and subscribe to chronic care plans for seamless, doorstep refills. ',
+                    style: AppStyles.w500gray20,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
