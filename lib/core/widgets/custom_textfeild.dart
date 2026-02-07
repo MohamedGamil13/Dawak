87d87d0d7eb70889e107aa5cca56269b0e7b93da
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-02-07 21:07:20 +0200

    add custom text feild

---
+++ lib/core/widgets/custom_textfeild.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class CustomTextfeild extends StatelessWidget {
+  const CustomTextfeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
